a6e5f68ee2dd489decdc74ef3d779862ad3f24b0
Nicolas Flores <nicolas.floress@mail.udp.cl>
2018-11-13 21:22:21 -0300

    aun no hace lo que quiero

--- casi final/cliente_chat.py
+++ casi final/cliente_chat.py
@@ -1,4 +1,9 @@
 import socket
+from _thread import *
+import threading
+
+def thread_rec(a):
+	print(a.decode())
 
 address='127.0.0.1'
 port= 5000
@@ -6,9 +11,13 @@ port= 5000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente: 
 	cliente.connect((address,port))
+	
 	while  True:
+
 		mensaje=input("Ingrese su mensaje: ") # se deberia elegir un nick cuando se entra al server
 		cliente.send(mensaje.encode()) 
+		threading.Thread(target=thread_rec, args=(cliente.recv(4096), )).start()
+		if not mensaje:
+			cliente.send("quit()".encode) 
+			break
 		if mensaje=="quit()": break
-		data=cliente.recv(4096) #recive sus propios mensaje, no de todos
-		print(data.decode())
